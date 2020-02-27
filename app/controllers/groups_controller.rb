4ae7dc2a58ceee31e54795a33d465991f616493a
nishimura246 <61145108+nishimura246@users.noreply.github.com>
2020-02-27 10:46:12 +0900

    送信機能実装

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,7 @@
 class GroupsController < ApplicationController
+
+  before_action :set_group, only: [:edit, :update]
+
   def index
   end
 
@@ -18,14 +21,12 @@ class GroupsController < ApplicationController
   end
 
   def edit
-    redirect_to group_messages_path(group.id)
   end
 
 
   def update
-    @group = Group.find(params[:id])
     if @group.update(group_params)
-      redirect_to root_path, notice: 'グループを更新しました'
+      redirect_to group_messages_path(@group), notice: 'グループを更新しました'
     else
       render :edit
     end
@@ -34,6 +35,10 @@ class GroupsController < ApplicationController
   private
   def group_params
     params.require(:group).permit(:name, user_ids: [])
+  end
+
+  def set_group
+    @group = Group.find(params[:id])
   end
   
 end
