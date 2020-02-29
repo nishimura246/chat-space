648834156206aa51219d7415d2dc989080b15491
nishimura246 <61145108+nishimura246@users.noreply.github.com>
2020-02-29 09:05:01 +0900

    非同期通信

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,24 @@
+$(function(){
+  function buildHTML(message){
+    if (message.image){
+      var html =
+      ``
+    } else {
+      var html = 
+    }
+    return html
+  }
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      type: 'POST',
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+  });
+});
